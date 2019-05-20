8ac1f0126fdf07d4e5016f7811427f292751bc6e
luowei <luowei@wodedata.com>
2019-05-20 14:38:05 +0800

    添加 UIApplicationUserDidTakeScreenshotNotification

--- Example/libSnapshot/LWViewController.m
+++ Example/libSnapshot/LWViewController.m
@@ -25,10 +25,21 @@
     [longPressGesture addTarget:self action:@selector(longPressGestureAction:)];
     [self.view addGestureRecognizer:longPressGesture];
 
+    //监听屏幕截图
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidTakeScreenshotNotification:) name:UIApplicationUserDidTakeScreenshotNotification object:nil];
 }
 
+- (void)appDidTakeScreenshotNotification:(NSNotification *)notification {
+    [self showSnapshotView];
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+
 - (IBAction)btnAction:(UIButton *)sender {
-    
+    [self showSnapshotView];
 }
 
 //手势截图分享
@@ -36,10 +47,15 @@
     NSLog(@"--------: 识别到手势截图");
 
     if(gesture.state == UIGestureRecognizerStateBegan ){
-        LWSnapshotMaskView *snapshotMaskView = [LWSnapshotMaskView showSnapshotMaskInView:self.view];
-        snapshotMaskView.snapshotFrame = CGRectMake((self.view.frame.size.width-200)/2, (self.view.frame.size.height-200)/2, 200, 200);
+        [self showSnapshotView];
     }
 
+}
+
+//显示截图
+- (void)showSnapshotView {
+    LWSnapshotMaskView *snapshotMaskView = [LWSnapshotMaskView showSnapshotMaskInView:self.view];
+    snapshotMaskView.snapshotFrame = CGRectMake((self.view.frame.size.width-200)/2, (self.view.frame.size.height-200)/2, 200, 200);
 }
 
 
